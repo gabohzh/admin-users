0507a85a3a4f1aef41e688febd59faa25690a109
Gabo Avalos <gabriel_hzh@hotmail.com>
2026-03-23 16:35:02 -0500

    Bootstrap, layout con menú y rutas base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { UserDetail } from './pages/user-detail/user-detail';
+import { UserForm } from './pages/user-form/user-form';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: Home },
+];
